f469991f862a25d85781e34a76e5a163ae4bc1c6
Abel Serrano Montiel <abesermon@alu.edu.gva.es>
2024-09-17 15:06:52 +0200

    Servidor ejemplo

---
+++ Servidor/Ejemplo clase 1/Ejemplo clase 1/Ejemplo 2.1.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Ejemplo_clase_1
+{
+    public partial class Ejemplo_2__1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnEnviar_Click(object sender, EventArgs e)
+        {
+           lblTexto.Text = txtNombre.Text;
+        }
+    }
+}
