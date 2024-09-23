d5cca374b1c028a047a25f47baee9211f7a7e09f
Abel Serrano Montiel <abesermon@alu.edu.gva.es>
2024-09-23 19:17:48 +0200

    Avance de ejercicio 2 servers

--- Servidor/Práctica 2/Acme/GesPresta/Empleados.aspx
+++ Servidor/Práctica 2/Acme/GesPresta/Empleados.aspx
@@ -1,5 +1,7 @@
 ﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Empleados.aspx.cs" Inherits="GesPresta.Empleados" %>
 
+<%@ Register src="Cabecera.ascx" tagname="Cabecera" tagprefix="uc1" %>
+
 <!DOCTYPE html>
 
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -11,6 +13,36 @@
 <body>
     <form id="form1" runat="server">
         <div>
+            <uc1:Cabecera ID="Cabecera1" runat="server" />
+        </div>
+        <div class="DatosEmpleados">
+            <h2>Datos de los Empleados</h2>
+        </div>
+        <div>
+            <label>Código Empleado</label>
+            <asp:TextBox ID="txtCodEmp" runat="server"></asp:TextBox> <br />
+            <label>NIF</label>
+            <asp:TextBox ID="txtNifEmp" runat="server"></asp:TextBox> <br />
+            <label>Apellidos y Nombre</label>
+            <asp:TextBox ID="txtNomEmp" runat="server"></asp:TextBox> <br />
+            <label>Dirección</label>
+            <asp:TextBox ID="txtDirEmp" runat="server"></asp:TextBox> <br />
+            <label>Ciudad</label>
+            <asp:TextBox ID="txtCiuEmp" runat="server"></asp:TextBox> <br />
+            <label>Teléfonos</label>
+            <asp:TextBox ID="txtTelEmp" runat="server"></asp:TextBox> <br />
+            <label>Fecha de Nacimiento</label>
+            <asp:TextBox ID="txtFnaEmp" runat="server"></asp:TextBox> <br />
+            <label>Fecha de Ingreso</label>
+            <asp:TextBox ID="txtFinEmp" runat="server"></asp:TextBox> <br />
+            <label>Sexo</label>
+            <asp:RadioButtonList ID="RadioButtonList1" runat="server" RepeatDirection="Horizontal">
+                <asp:ListItem Selected="True" Value="H">Hombre</asp:ListItem>
+                <asp:ListItem Value="M">Mujer</asp:ListItem>
+            </asp:RadioButtonList>&nbsp;<br />
+            <label>Departamento</label>
+            <asp:DropDownList ID="ddlDepEmp" runat="server"></asp:DropDownList> <br />
+            <asp:Button ID="cmdEnviar" runat="Server" Text="Enviar"/>
         </div>
     </form>
 </body>
--- Servidor/Práctica 2/Acme/GesPresta/Empleados.aspx.designer.cs
+++ Servidor/Práctica 2/Acme/GesPresta/Empleados.aspx.designer.cs
@@ -22,5 +22,113 @@ namespace GesPresta
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Control Cabecera1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::GesPresta.Cabecera Cabecera1;
+
+        /// <summary>
+        /// Control txtCodEmp.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtCodEmp;
+
+        /// <summary>
+        /// Control txtNifEmp.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNifEmp;
+
+        /// <summary>
+        /// Control txtNomEmp.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNomEmp;
+
+        /// <summary>
+        /// Control txtDirEmp.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtDirEmp;
+
+        /// <summary>
+        /// Control txtCiuEmp.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtCiuEmp;
+
+        /// <summary>
+        /// Control txtTelEmp.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtTelEmp;
+
+        /// <summary>
+        /// Control txtFnaEmp.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtFnaEmp;
+
+        /// <summary>
+        /// Control txtFinEmp.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtFinEmp;
+
+        /// <summary>
+        /// Control RadioButtonList1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButtonList RadioButtonList1;
+
+        /// <summary>
+        /// Control ddlDepEmp.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlDepEmp;
+
+        /// <summary>
+        /// Control cmdEnviar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button cmdEnviar;
     }
 }
